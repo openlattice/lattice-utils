19e411d5b0ded850c415344212817ff07d59398d
Hristo Oskov <hristo@openlattice.com>
2020-05-08 12:44:36 -0700

    formatting

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -5,7 +5,9 @@
 import { useCallback, useState } from 'react';
 
 const useInput = (defaultValue :any, isNumeric ?:boolean = false) => {
+
   const [inputValue, setValue] = useState(defaultValue);
+
   const setInput = useCallback((event :SyntheticEvent<HTMLInputElement>) => {
     const { value } = event.currentTarget;
     const nextValue = isNumeric ? parseFloat(value) : value;
