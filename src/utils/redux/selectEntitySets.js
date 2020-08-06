d7dfefa529496fdb6d194f3da15c30898e8e49a4
Hristo Oskov <hristo@openlattice.com>
2020-08-06 11:20:48 -0700

    selectEntitySets tests

--- src/utils/redux/selectEntitySets.js
+++ src/utils/redux/selectEntitySets.js
@@ -2,14 +2,19 @@
  * @flow
  */
 
-import { Map, Set, isCollection } from 'immutable';
+import {
+  Map,
+  Set,
+  getIn,
+  isCollection,
+} from 'immutable';
 
 import { EDM, ENTITY_SETS, ENTITY_SETS_INDEX_MAP } from '../../constants/redux';
 import { isNonEmptyArray, isNonEmptyString } from '../lang';
 import { isValidUUID } from '../validation';
 import type { UUID } from '../../types';
 
-export default function selectEntitySets<EntitySet>(idsOrNames :Array<UUID | string> | Set<UUID | string>) {
+export default function selectEntitySets<EntitySet>(idsOrNames :Set<UUID | string> | Array<UUID | string>) {
 
   return (state :Map) :Map<UUID, EntitySet> => {
 
@@ -26,9 +31,9 @@ export default function selectEntitySets<EntitySet>(idsOrNames :Array<UUID | str
 
     const entitySetsMap = Map().withMutations((map :Map) => {
       idsOrNames.forEach((idOrName) => {
-        const entitySetIndex :number = state.getIn([EDM, ENTITY_SETS_INDEX_MAP, idOrName], -1);
+        const entitySetIndex :number = getIn(state, [EDM, ENTITY_SETS_INDEX_MAP, idOrName], -1);
         if (entitySetIndex >= 0) {
-          const entitySet :?EntitySet = state.getIn([EDM, ENTITY_SETS, entitySetIndex]);
+          const entitySet :?EntitySet = getIn(state, [EDM, ENTITY_SETS, entitySetIndex]);
           if (entitySet && entitySet.id) {
             map.set(entitySet.id, entitySet);
           }
