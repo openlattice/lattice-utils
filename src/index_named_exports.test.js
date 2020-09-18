c86a64f63e6296056f0b12bcca628ae278cd17a9
Hristo Oskov <hristo@openlattice.com>
2020-09-18 15:31:43 -0700

    selectEntityTypes

--- src/index_named_exports.test.js
+++ src/index_named_exports.test.js
@@ -14,7 +14,7 @@ const EXPECTED_OBJ_EXPORTS = OrderedMap({
   DataUtils       : { size: 1 },
   LangUtils       : { size: 7 },
   PersonUtils     : { size: 1 },
-  ReduxUtils      : { size: 3 },
+  ReduxUtils      : { size: 4 },
   RoutingUtils    : { size: 1 },
   ValidationUtils : { size: 1 },
   WebCryptoUtils  : { size: 4 },
