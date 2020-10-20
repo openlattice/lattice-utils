8880e10a67e22f7abb737db7f6082ebcf5a6ec14
Giulia Campana <giuliaecampana@gmail.com>
2020-10-20 16:17:57 -0700

    update index.js and tests

--- src/utils/data/index.js
+++ src/utils/data/index.js
@@ -3,3 +3,5 @@
  */
 
 export { default as getEntityKeyId } from './getEntityKeyId';
+export { default as getPropertyValue } from './getPropertyValue';
+export { default as getPropertyValues } from './getPropertyValues';
