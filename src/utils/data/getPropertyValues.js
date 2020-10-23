ebc23416752e20c2e63815ccf2b6f5e35f79b824
Giulia Campana <giuliaecampana@gmail.com>
2020-10-22 19:37:33 -0700

    update getPropertyValues and tests

--- src/utils/data/getPropertyValues.js
+++ src/utils/data/getPropertyValues.js
@@ -2,7 +2,7 @@
  * @flow
  */
 
-import { Map } from 'immutable';
+import { Map, set } from 'immutable';
 import { Models } from 'lattice';
 
 import getPropertyValue from './getPropertyValue';
@@ -11,9 +11,16 @@ const { FQN } = Models;
 
 export default function getPropertyValues(
   entity :Map | Object,
-  fqns :any[],
-  defaultValue ? :string = ''
-) :any {
+  fqns :Array<FQN | string>,
+  fallback ?:boolean | number | string = '',
+) :Object {
 
-  return fqns.map((fqn :FQN | string) => getPropertyValue(entity, fqn, defaultValue));
+  let propertyValues = {};
+
+  fqns.forEach((fqn :FQN | string) => {
+    const value = getPropertyValue(entity, fqn, fallback);
+    propertyValues = set(propertyValues, fqn, value);
+  });
+
+  return propertyValues;
 }
--- src/utils/data/getPropertyValues.test.js
+++ src/utils/data/getPropertyValues.test.js
@@ -2,64 +2,41 @@
  * @flow
  */
 
-import { List, Map } from 'immutable';
-import { Models } from 'lattice';
 import { DateTime } from 'luxon';
 
 import getPropertyValues from './getPropertyValues';
 
-import { INVALID_PARAMS } from '../../testing/InvalidParams';
-
-const { FQN } = Models;
-
 const MOCK_NAME_VALUE = 'Test';
 const MOCK_DATETIME_VALUE = DateTime.local().toISO();
-const MOCK_NAME_FQN_PROPERTY = FQN.of('ol.name');
-const MOCK_DATETIME_FQN_PROPERTY = FQN.of('ol.datetime');
+const MOCK_NAME_PROPERTY = 'ol.name';
+const MOCK_DATETIME_PROPERTY = 'ol.datetime';
 
-const MOCK_ENTITY_MAP_USING_FQN = Map().withMutations((mutator) => {
-  mutator.set(MOCK_NAME_FQN_PROPERTY, List([MOCK_NAME_VALUE]));
-  mutator.set(MOCK_DATETIME_FQN_PROPERTY, List([MOCK_DATETIME_VALUE]));
-});
-const MOCK_ENTITY_SINGLE_VALUE_MAP_USING_FQN = Map().set(MOCK_NAME_FQN_PROPERTY, List([MOCK_NAME_VALUE]));
+const MOCK_ENTITY_OBJ = {
+  [MOCK_DATETIME_PROPERTY]: MOCK_DATETIME_VALUE,
+  [MOCK_NAME_PROPERTY]: MOCK_NAME_VALUE,
+};
 
 describe('DataUtils', () => {
 
   describe('getPropertyValues', () => {
 
-    test('should return the property value when defined', () => {
-
-      expect(getPropertyValues(
-        MOCK_ENTITY_MAP_USING_FQN,
-        [MOCK_NAME_FQN_PROPERTY, MOCK_DATETIME_FQN_PROPERTY]
-      )).toEqual([MOCK_NAME_VALUE, MOCK_DATETIME_VALUE]);
-
-      expect(getPropertyValues(
-        MOCK_ENTITY_MAP_USING_FQN,
-        [MOCK_NAME_FQN_PROPERTY]
-      )).toEqual([MOCK_NAME_VALUE]);
+    test('should return an object of with key/value pairs of fqn/value for each fqn provided', () => {
+      expect(getPropertyValues(MOCK_ENTITY_OBJ, [MOCK_NAME_PROPERTY, MOCK_DATETIME_PROPERTY])).toEqual(MOCK_ENTITY_OBJ);
     });
 
-    test('should return the default value when property value is undefined', () => {
-      expect(getPropertyValues({}, [MOCK_DATETIME_FQN_PROPERTY])).toEqual(['']);
+    test('should return an object with the default value for every undefined property', () => {
+      expect(getPropertyValues({}, [MOCK_NAME_PROPERTY])).toEqual({ [MOCK_NAME_PROPERTY]: '' });
+      expect(getPropertyValues({}, [MOCK_NAME_PROPERTY], 'Default')).toEqual({ [MOCK_NAME_PROPERTY]: 'Default' });
       expect(getPropertyValues(
-        MOCK_ENTITY_SINGLE_VALUE_MAP_USING_FQN,
-        [MOCK_DATETIME_FQN_PROPERTY]
-      )).toEqual(['']);
-
-      expect(getPropertyValues({}, [MOCK_DATETIME_FQN_PROPERTY], 'Default')).toEqual(['Default']);
+        { [MOCK_DATETIME_PROPERTY]: [MOCK_DATETIME_VALUE] },
+        [MOCK_NAME_PROPERTY]
+      )).toEqual({ [MOCK_NAME_PROPERTY]: '' });
       expect(getPropertyValues(
-        MOCK_ENTITY_SINGLE_VALUE_MAP_USING_FQN,
-        [MOCK_DATETIME_FQN_PROPERTY],
+        { [MOCK_DATETIME_PROPERTY]: [MOCK_DATETIME_VALUE] },
+        [MOCK_NAME_PROPERTY],
         'Default'
-      )).toEqual(['Default']);
+      )).toEqual({ [MOCK_NAME_PROPERTY]: 'Default' });
     });
-
-    test('should return the default value when given invalid paramters', () => {
-      expect(getPropertyValues({}, INVALID_PARAMS)).toEqual(INVALID_PARAMS.map(() => ''));
-      expect(getPropertyValues({}, INVALID_PARAMS, 'Default')).toEqual(INVALID_PARAMS.map(() => 'Default'));
-    });
-
   });
 
 });
