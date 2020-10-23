15cb0cfe0320ebc5a7fa7dcc51cc18c87f7b844a
Giulia Campana <giuliaecampana@gmail.com>
2020-10-22 19:10:39 -0700

    remove getPropertyMultiplicityValue from index

--- src/utils/data/index.js
+++ src/utils/data/index.js
@@ -3,6 +3,5 @@
  */
 
 export { default as getEntityKeyId } from './getEntityKeyId';
-export { default as getPropertyMultiplicityValue } from './getPropertyMultiplicityValue';
 export { default as getPropertyValue } from './getPropertyValue';
 export { default as getPropertyValues } from './getPropertyValues';
