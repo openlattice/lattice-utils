fd015dea5ce2e62d30326b583f74b50b245f1839
Todd Bergman <todd@openlattice.com>
2020-10-29 14:44:12 -0600

    export formatAsRelative

--- src/utils/datetime/index.js
+++ src/utils/datetime/index.js
@@ -5,4 +5,5 @@
 export { default as calculateAge } from './calculateAge';
 export { default as formatAsDate } from './formatAsDate';
 export { default as formatAsTime } from './formatAsTime';
+export { default as formatAsRelative } from './formatAsRelative';
 export { default as formatDateTime } from './formatDateTime';
