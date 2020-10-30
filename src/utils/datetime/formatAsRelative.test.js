4d1ad51ed8e23e2761dd1619915cf5792ad2c0f0
Todd Bergman <todd@openlattice.com>
2020-10-30 11:50:02 -0600

    update relative time

--- src/utils/datetime/formatAsRelative.test.js
+++ src/utils/datetime/formatAsRelative.test.js
@@ -5,8 +5,7 @@
 import { DateTime } from 'luxon';
 
 import formatAsRelative from './formatAsRelative';
-import formatDateTime from './formatDateTime';
-import { INVALID_DATE_TIME, RELATIVE_TIMES } from './constants';
+import { RELATIVE_TIMES } from './constants';
 import { INVALID_PARAMS } from '../../testing/InvalidParams';
 
 describe('DateTimeUtils', () => {
@@ -17,9 +16,11 @@ describe('DateTimeUtils', () => {
     const threeDaysAgo = today.minus({ days: 3 }).toISO();
     const expectedThreeDaysAgoLabel = '3 days ago';
     const threeWeeksAgo = today.minus({ weeks: 3 }).toISO();
-    const expectedThreeWeeksAgoLabel = '3 weeks ago';
+    const expectedThreeWeeksAgoLabel = '21 days ago';
     const threeMonthsAgo = today.minus({ months: 3 }).toISO();
     const expectedThreeMonthsAgoLabel = '3 months ago';
+    const threeYearsAgo = today.minus({ years: 3 }).toISO();
+    const expectedThreeYearsAgoLabel = '3 years ago';
 
     test('should return Today and Yesterday Labels for today and yesterday', () => {
       expect(formatAsRelative(today.toISO())).toEqual(RELATIVE_TIMES.TODAY);
@@ -34,26 +35,15 @@ describe('DateTimeUtils', () => {
     test('should return units of months if at least one month ago and less than one year ago', () => {
       expect(formatAsRelative(threeMonthsAgo)).toEqual(expectedThreeMonthsAgoLabel);
     });
-
-    test('should return actual date in default DATE_SHORT format if over a year ago', () => {
-      expect(formatAsRelative('2017-06-29T12:34:56')).toEqual(
-        formatDateTime('2017-06-29T12:34:56', DateTime.DATE_SHORT)
-      );
-    });
-    test('should return actual date in specified format if over a year ago', () => {
-      expect(formatAsRelative('2017-06-29T12:34:56', DateTime.DATETIME_SHORT)).toEqual(
-        formatDateTime('2017-06-29T12:34:56', DateTime.DATETIME_SHORT)
-      );
+    test('should return units of years if at least one year ago', () => {
+      expect(formatAsRelative(threeYearsAgo)).toEqual(expectedThreeYearsAgoLabel);
     });
 
     test('should correctly handle invalid parameters', () => {
       INVALID_PARAMS.forEach((invalidParam) => {
-        expect(formatAsRelative(invalidParam)).toEqual(INVALID_DATE_TIME);
+        expect(formatAsRelative(invalidParam, '')).toEqual('');
         // $FlowFixMe - ignore for test
-        expect(formatAsRelative([invalidParam])).toEqual(INVALID_DATE_TIME);
-        expect(formatAsRelative(invalidParam, DateTime.DATE_SHORT, '')).toEqual('');
-        // $FlowFixMe - ignore for test
-        expect(formatAsRelative([invalidParam], DateTime.DATE_SHORT, '')).toEqual('');
+        expect(formatAsRelative([invalidParam], '')).toEqual('');
       });
     });
 
