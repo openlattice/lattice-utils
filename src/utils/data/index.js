672e1e3af1bb3c61a9f92f9f0071824b364a1056
Giulia Campana <giuliaecampana@gmail.com>
2020-10-29 17:03:38 -0700

    adjust exports

--- src/utils/data/index.js
+++ src/utils/data/index.js
@@ -4,4 +4,3 @@
 
 export { default as getEntityKeyId } from './getEntityKeyId';
 export { default as getPropertyValue } from './getPropertyValue';
-export { default as getPropertyValues } from './getPropertyValues';
